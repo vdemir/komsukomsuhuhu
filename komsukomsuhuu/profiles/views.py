c016efc61ca6fd2bb943384fe4504c524ed189e9
Bünyamin Atik <bunyamin.atik1@gmail.com>
2015-04-25 19:50:36 +0300

    groups and entities permissions added

--- komsukomsuhuu/profiles/views.py
+++ komsukomsuhuu/profiles/views.py
@@ -148,19 +148,3 @@ def notifications(request):
         'notifications': notifications
     }, RequestContext(request))
 
-def reset(request):
-
-    return password_reset(request, template_name='reset.html',
-        email_template_name='reset_email.html',
-        subject_template_name='reset_subject.txt',
-        post_reset_redirect=reverse('success'))
-
-
-def reset_confirm(request, uidb64=None, token=None):
-
-    return password_reset_confirm(request, template_name='reset_confirm.html',
-        uidb36=uidb36, token=token, post_reset_redirect=reverse('success'))
-
-
-def success(request):
-  return render(request, "success.html")
