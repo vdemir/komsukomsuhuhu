5b5e07f71c951e36bab71e048077666ba76e4e8d
Ahmet ERKOC <aerkoc01@gmail.com>
2015-05-09 19:19:40 +0300

    Unnecesary comma deleted

--- komsukomsuhuu/groups/forms.py
+++ komsukomsuhuu/groups/forms.py
@@ -5,27 +5,24 @@ from models import Group, GroupLocation
 
 
 class GroupForm(forms.ModelForm):
-
     class Meta:
         fields = ['name', 'type', 'state', 'range', 'duration', 'enrollment_key']
         model = Group
 
 
 class EditGroupForm(forms.ModelForm):
-
     class Meta:
-        fields = ['name', 'type', 'range',]
+        fields = ['name', 'type', 'range']
         model = Group
 
 
 class GroupLocationForm(forms.ModelForm):
-
     class Meta:
         fields = ['longitude', 'latitude']
         model = GroupLocation
 
-class CheckEnrollmentKey(forms.ModelForm):
 
+class CheckEnrollmentKey(forms.ModelForm):
     class Meta:
         fields = ['enrollment_key']
         model = Group
