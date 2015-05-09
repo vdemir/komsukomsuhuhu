18885a4cd00bc7e08bac50f7a76c20a48f58edfb
Ahmet ERKOC <aerkoc01@gmail.com>
2015-05-09 19:52:43 +0300

    Redirection and give information to user fix

--- komsukomsuhuu/messages/views.py
+++ komsukomsuhuu/messages/views.py
@@ -12,19 +12,17 @@ from functions.function import info
 
 
 def send_notification(sender, recipient_username, verb, target):
-
     if recipient_username:
-                notify.send(
-                    sender,
-                    recipient=recipient_username,
-                    verb=verb,
-                    target=target,
-                )
+        notify.send(
+            sender,
+            recipient=recipient_username,
+            verb=verb,
+            target=target,
+        )
 
 
 @login_required(login_url='/login')
 def new_message(request, username):
-
     user = get_object_or_404(User, username=username)
 
     form = NewMessageForm(request.POST)
@@ -36,7 +34,6 @@ def new_message(request, username):
         conversation.users.add(request.user)
         conversation.users.add(user)
 
-
         message = conversation.messages.create(
             text=text,
             sender=request.user,
@@ -44,21 +41,23 @@ def new_message(request, username):
 
         message.seen_users.add(request.user)
 
-        '''
-        redirect_to = "%(path)s?message_sent=true" % {
-            "path": reverse("detail_advert", args=[pk])
-        }
-        '''
-
         send_notification(request.user, user, "created new conversation", conversation)
 
-        return HttpResponse("Gonderildi")
+        redirect_to = "%(path)s?message_sent=success" % {
+            "path": reverse("inbox")
+        }
 
-    return HttpResponse("gonderilmedi")
+        return redirect(redirect_to)
+
+    redirect_to = "%(path)s?message_sent=error" % {
+        "path": reverse("inbox")
+    }
+
+    return redirect(redirect_to)
+
 
 @login_required(login_url='/login')
 def inbox(request):
-
     conversations = Conversation.objects.filter(
         users=request.user,
     ).annotate(
@@ -68,19 +67,20 @@ def inbox(request):
     ).order_by(
         "-date_created"
     )
+    message_sent = request.GET.get("message_sent")
 
     return render_to_response("inbox.html", {
         'favorited_groups': info(request)[0],
         'favorited_topics': info(request)[1],
         'notifications': info(request)[2],
         'inbox_notifications': info(request)[3],
-        "conversations": conversations
+        'conversations': conversations,
+        'message_sent': message_sent
     }, RequestContext(request))
 
 
 @login_required(login_url='/login')
 def conversation_detail(request, pk):
-
     conversation = get_object_or_404(
         Conversation, users=request.user, id=pk)
 
@@ -125,9 +125,8 @@ def conversation_detail(request, pk):
     for unread_notification in unread_notifications:
         if unread_notification.target == conversation:
             unread_notification.mark_as_read()
-            unread_notification.level="info"
+            unread_notification.level = "info"
             unread_notification.save()
-
 
     return render_to_response("conversation_detail.html", {
         'favorited_groups': info(request)[0],
